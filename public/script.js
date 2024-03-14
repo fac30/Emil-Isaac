f9edcfff78d1134a34f45d773d5ef6c8ca24320c
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2024-03-14 21:30:39 +0000

    resolved conflicts and udated files index.html and script.js

--- public/script.js
+++ public/script.js
@@ -1,43 +1,48 @@
 //import findingMuseum from "../utils/findMuseum";
 
 const Button = document.getElementById("buttonFind");
-Button.addEventListener("click", findMuseumMapHandler(e));
+Button.addEventListener("click", findMuseumMapHandler);
+const MapContainer = document.getElementById("map-container");
+const postCodeVal = document.getElementById("post-code").value;
 
-function findMuseumMapHandler(e) {
-  e.preventDefault(); // Prevent default form submission
-
-  const postCode = document.getElementById("post-code").value;
-  mapContainer.style.display = "block";
+function findMuseumMapHandler() {
+  //e.preventDefault(); // Prevent default form submission
+  if ((MapContainer.style.display = "none")) {
+    MapContainer.style.display = "block";
+    postCodeVal = "";
+  } else {
+    MapContainer.style.display = "none";
+  }
 
   // Send a POST request to the Express server
-  fetch("/api/search", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ postCode: postCode }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      // Handle response data and display it in the map-container div
-      var mapContainer = document.getElementById("map-container");
-      //mapContainer.style.display = "block";
-      mapContainer.innerHTML = ""; // Clear previous content
+  //   fetch("/api/search", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({ postCode: postCode }),
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       // Handle response data and display it in the map-container div
+  //       var mapContainer = document.getElementById("map-container");
+  //       //mapContainer.style.display = "block";
+  //       mapContainer.innerHTML = ""; // Clear previous content
 
-      // Iterate over the results and display them
-      data.results.forEach(function (place) {
-        var placeName = place.name;
-        var placeAddress = place.vicinity;
+  //       // Iterate over the results and display them
+  //       data.results.forEach(function (place) {
+  //         var placeName = place.name;
+  //         var placeAddress = place.vicinity;
 
-        var placeElement = document.createElement("div");
-        placeElement.textContent = placeName + " - " + placeAddress;
+  //         var placeElement = document.createElement("div");
+  //         placeElement.textContent = placeName + " - " + placeAddress;
 
-        mapContainer.appendChild(placeElement);
-      });
+  //         mapContainer.appendChild(placeElement);
+  //       });
 
-      // Make the map container visible
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error);
-    });
+  //       // Make the map container visible
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching data:", error);
+  //     });
 }
